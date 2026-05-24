6296daec5dcf1359acf1cdad404afded2004595a
snapsend-chat <llc.bytewave@gmail.com>
2026-05-24 07:13:02 +0100

    signin update

--- authentication/FollowUsers.js
+++ authentication/FollowUsers.js
@@ -5,9 +5,25 @@ const FollowUsers = (app, db, requireAuth, getUserByEmail) => {
       console.log(y)
       if(!y?.code) {
         let u = await getUserByEmail(y.email);
+        let usersToFollow = await db.ref("users/").once("value");
+        if(!usersToFollow.exists()) res.json({ users: []});
+        usersToFollow = usersToFollow.val();
+        const uk = Object.keys(usersToFollow);
+        const v = uk.filter(t => t != y.id);
+        let filteredUsers = [];
+        if(v.length <= 0) res.json({ users: []});
+        v.forEach((u, i) => {
+          let { username, data, user_number } = usersToFollow[u];
+          let pic = data.profile_pictures;
+          pic = Object.values(pic);
+          pic = pic.length > 0 ? pic[pic.length-1] : null;
+          filteredUsers.push({username, user_number, pic, id: u})
+        })
+        console.log(filteredUsers)
+        //console.log(usersToFollow[v])
         if(u) {
           let loc = u.location;
-          res.json({ message: loc});
+          res.json(filteredUsers);
         }
       }
     } catch(err) {
--- authentication/SignIn.js
+++ authentication/SignIn.js
@@ -1,17 +1,19 @@
-const SignIn = (app, db, encode, generateCellNumber, generateSessionToken, encrypt) => {
+const SignIn = (app, db, encode, generateSessionToken, encrypt) => {
   app.post("/api/signin", async (req, res) => {
+    const { email, password } = req.body;
     try {
-      const { email, password } = req.body;
       const st = generateSessionToken(email, encrypt);
       const r = (await db.ref("users/").once("value")).val();
       if(!r) res.status(404).json({ error: "User not found" });
       let usersObj = Object.values(r);
       usersObj = usersObj.find(t => t.email == email && t.auth.password == encrypt(password));
+      //console.log(usersObj)
       if(!usersObj) res.status(500).json({ error: "User not found" });
       const { state } = usersObj.auth;
       const authToken = encode(usersObj.email, password);
       res.json({ message: "Login successfully", auth_token: authToken, sessionToken: st, state});
     } catch (err) {
+      console.log({error: "Incorrect credentials"})
       res.status(404).json({error: "Incorrect credentials"});
     }
   })
